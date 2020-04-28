7f602642c9985e4969ee51e5af1ed4154ead79a0
codekaust <kaustubh1303@gmail.com>
2020-04-28 20:01:35 +0530

    added feature: client user names

--- dev-kaustubh/server/server.cpp
+++ dev-kaustubh/server/server.cpp
@@ -6,20 +6,27 @@
 #include <arpa/inet.h> 
 
 #include<iostream>
-#include <string>
 #include <fstream>
+
+#include <string>
+#include <map>
 
 #define PORT 3001
 #define BUF_SIZE 1024
 
+using namespace std;
+
+//maps client socket with his username 
+map<int, string> map_client_username;
+
 //test command according to string
-bool checkCommand(int length, std::string command, int valread, char* buffer){
+bool checkCommand(int length, string command, int valread, char* buffer){
     //this check is unnecessary, but will increase performance
     if(valread==length){
         //this should be already done, still doing again
         buffer[valread] = '\0';
 
-        std::string s = buffer;
+        string s = buffer;
         if(s==command){
             return true;
         }else{
@@ -119,7 +126,11 @@ int main(){
         activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   
        
         if ((activity < 0) && (errno!=EINTR))
-            puts("select error");  
+            puts("select error"); 
+
+            
+        //This msg_string will be used later to inform all clients about the activity as required.
+        string msg_string; 
              
         //request at master socket -> accept request
         if (FD_ISSET(master_socket, &readfds))   
@@ -130,14 +141,10 @@ int main(){
                 exit(EXIT_FAILURE);   
             }   
              
-            //print new connection details
-            printf("New connection details: socket_fd is %d , ip is : %s , port : %d\n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
-           
             //send greeting and inform client of its socket_fd number (which serves as its name)
             char greeting[BUF_SIZE];
-            sprintf(greeting, "Successfully connected with server.\nYour socket_fd number (serves as name) is %d.\n", new_socket);  
-            if(send(new_socket, greeting, strlen(greeting), 0) != strlen(greeting))      
-                perror("send");   
+            sprintf(greeting, "Successfully connected with server.\nYour socket_fd number is %d.\n", new_socket);  
+            send(new_socket, greeting, strlen(greeting), 0) != strlen(greeting);
                  
             //add new socket to array of sockets  
             for (int i = 0; i < max_clients; i++)   
@@ -149,122 +156,141 @@ int main(){
                     break;   
                 }   
             }   
+
+            //get the new client username and inform all other clients
+            int valread = read(new_socket, buffer, BUF_SIZE);
+            buffer[valread] = '\0';
+
+            //add new client to map
+            map_client_username[new_socket] = buffer;
+
+            //used for sending message to other clients
+            sd = new_socket;
+            msg_string = map_client_username[new_socket] + ": I have joined this chat room.";
+
+            //print new connection details
+            printf("New connection details: socket_fd is %d , ip is : %s , port : %d\n, username: %s.\n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port), buffer);
         }   
-             
-        //else its some IO operation on some other socket 
-        for (int i = 0; i < max_clients; i++)   
-        {   
-            //sd should be socket on which the IO operation has happened
-            sd = client_sockets[i];   
-                 
-            if (FD_ISSET(sd , &readfds))   
+
+        else{ 
+            //else its some IO operation on some other socket 
+            for (int i = 0; i < max_clients; i++)   
             {   
-                //Read the incoming message  
-                valread = read( sd , buffer, BUF_SIZE);
-
-                //mark string termination in buffer
-                buffer[valread] = '\0';
-
-                //Check for connection closing request 
-                if (valread == 0)   
+                //sd should be socket on which the IO operation has happened
+                sd = client_sockets[i];   
+                    
+                if (FD_ISSET(sd , &readfds))   
                 {   
-                    //Somebody disconnected , get his details and print  
-                    printf("Client disconnected with socket_fd %d.", sd);   
-                         
-                    //Close the socket and mark as 0 in list for reuse  
-                    close(sd);
-                    client_sockets[i] = 0;   
-                }
-                    
-                else
-                {   
-                    //print incoming message details
-                    printf("Recieved message: %s, from socket_fd: %d\n", buffer, sd);
-                    
-                    //This msg_string will be used later to inform all clients about the activity.
-                    std::string msg_string;
+                    //Read the incoming message  
+                    valread = read( sd , buffer, BUF_SIZE);
 
-                    //A client is sending a file to server
-                    if(isIncomingFile(valread, buffer)){
-                        //firstly get the file name
-                        char file_name[BUF_SIZE];
-                        valread = read(sd, file_name, BUF_SIZE);
-                        file_name[valread] = '\0';
+                    //mark string termination in buffer
+                    buffer[valread] = '\0';
 
-                        //copy required as file_name is lost while writing file
-                        std::string file_name_copy = file_name;
+                    //Check for connection closing request 
+                    if (valread == 0)   
+                    {   
+                        //Somebody disconnected , get his details and print  
+                        printf("Client disconnected with socket_fd %d.", sd);   
+                            
+                        //Close the socket and mark as 0 in list for reuse  
+                        close(sd);
+                        client_sockets[i] = 0;  
 
-                        //checks for error on client side in which it may disconnect
-                        if(valread!=0){
-                            std::ofstream out(file_name, std::ios_base::out | std::ios_base::binary);
+                        //to tell all other clients
+                        msg_string = map_client_username[sd] + ": Left chat room.";
 
-                            //start reading file data from socket and write in a new file
-                            while(true) {
-                                valread = read(sd, buffer, BUF_SIZE);
-                                buffer[valread] = '\0';
-
-                                //recieved command telling file recieved completely in previous iteration
-                                if(isFileRecieved(valread, buffer)){
-                                    break;
-                                }else{
-                                    out.write(&buffer[0], BUF_SIZE);
-                                }
-                            }        
-
-                            out.close(); 
-                        } 
-
-                        msg_string = "Client "+std::to_string(sd)+" sent file: " + file_name_copy;
-                        std::cout<<"Recieved file " + file_name_copy + " from client: " + std::to_string(sd)<<std::endl;
+                        //delete entry from map
+                        map_client_username.erase(sd);
                     }
+                        
+                    else
+                    {   
+                        //print incoming message details
+                        printf("Recieved message: %s, from socket_fd: %d\n", buffer, sd);
 
-                    //A client has requested a file from server
-                    else if(isFileRequested(valread, buffer)){
-                        //firstly get the file name of file being requested
-                        char file_name[BUF_SIZE];
-                        valread = read(sd, file_name, BUF_SIZE);
-                        file_name[valread] = '\0';
+                        //A client is sending a file to server
+                        if(isIncomingFile(valread, buffer)){
+                            //firstly get the file name
+                            char file_name[BUF_SIZE];
+                            valread = read(sd, file_name, BUF_SIZE);
+                            file_name[valread] = '\0';
 
-                        //copy required as file_name is lost while writing file
-                        std::string file_name_copy = file_name;
+                            //copy required as file_name is lost while writing file
+                            string file_name_copy = file_name;
 
-                        //for reading file
-                        std::ifstream in(file_name, std::ios_base::in | std::ios_base::binary);
+                            //checks for error on client side in which it may disconnect
+                            if(valread!=0){
+                                ofstream out(file_name, ios_base::out | ios_base::binary);
 
-                        do{
-                            in.read(&buffer[0], BUF_SIZE);      // Read at most n bytes into
-                            send(sd, buffer, BUF_SIZE, 0);
-                        } while (in.gcount() > 0);  
+                                //start reading file data from socket and write in a new file
+                                while(true) {
+                                    valread = read(sd, buffer, BUF_SIZE);
+                                    buffer[valread] = '\0';
 
-                        in.close();
+                                    //recieved command telling file recieved completely in previous iteration
+                                    if(isFileRecieved(valread, buffer)){
+                                        break;
+                                    }else{
+                                        out.write(&buffer[0], BUF_SIZE);
+                                    }
+                                }        
 
-                        //Inform Client that file sent completely
-                        std::string sent_file_command = "/file sent";
-                        send(sd, (char *)sent_file_command.c_str(), strlen((char *)sent_file_command.c_str()), 0);
+                                out.close(); 
+                            } 
 
-                        std::cout<<"Sent file " + file_name_copy + " to client: " + std::to_string(sd)<<std::endl;
-                    }
-                    
-                    //A client has sent a text message
-                    else{
-                        msg_string = "Client "+std::to_string(sd)+": "+buffer;
-                    }
-
-                    //Inform all clients about the activity | send them msg_string
-                    char* msg = (char *)msg_string.c_str();
-
-                    //send incoming message details to all clients
-                    //socket_fd (serves as client name) is added in string showing from which client the request has been recieved
-                    for(int j=0; j<max_clients; j++){
-                        int temp_sd = client_sockets[j];
-
-                        if(temp_sd != sd && temp_sd!=0){
-                            send(temp_sd, msg, strlen(msg), 0 );   
+                            msg_string = map_client_username[sd]+": sent file - " + file_name_copy;
+                            cout<<"Recieved file " + file_name_copy + " from client: " + to_string(sd)<<endl;
                         }
-                    }
+
+                        //A client has requested a file from server
+                        else if(isFileRequested(valread, buffer)){
+                            //firstly get the file name of file being requested
+                            char file_name[BUF_SIZE];
+                            valread = read(sd, file_name, BUF_SIZE);
+                            file_name[valread] = '\0';
+
+                            //copy required as file_name is lost while writing file
+                            string file_name_copy = file_name;
+
+                            //for reading file
+                            ifstream in(file_name, ios_base::in | ios_base::binary);
+
+                            do{
+                                in.read(&buffer[0], BUF_SIZE);      // Read at most n bytes into
+                                send(sd, buffer, BUF_SIZE, 0);
+                            } while (in.gcount() > 0);  
+
+                            in.close();
+
+                            //Inform Client that file sent completely
+                            string sent_file_command = "/file sent";
+                            send(sd, (char *)sent_file_command.c_str(), strlen((char *)sent_file_command.c_str()), 0);
+
+                            cout<<"Sent file " + file_name_copy + " to client: " + to_string(sd)<<endl;
+                        }
+                        
+                        //A client has sent a text message
+                        else{
+                            msg_string = map_client_username[sd]+": "+buffer;
+                        }
+                    }   
                 }   
-            }   
-        }   
+            }
+        }
+
+        //Inform all clients about the activity, i.e. send them msg_string (as required)
+        char* msg = (char *)msg_string.c_str();
+
+        //send incoming message details to all clients
+        //socket_fd (serves as client name) is added in string showing from which client the request has been recieved
+        for(int j=0; j<max_clients; j++){
+            int temp_sd = client_sockets[j];
+
+            if(temp_sd != sd && temp_sd!=0){
+                send(temp_sd, msg, strlen(msg), 0 );   
+            }
+        }  
     }   
          
     return 0;   
